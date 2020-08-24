5feefa880c5bd75e013dbdab6b3cc2f0dc86b34c
masariuman <masariuman@gmail.com>
2020-08-24 13:51:55 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/template_partial/routeAjax.blade.php
+++ resources/views/admin/template_partial/routeAjax.blade.php
@@ -1,7 +1,7 @@
 <script>
     function routeToRuangan() {
-        event.preventDefault();
-        const CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content')
+        // event.preventDefault();
+        // const CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content')
 
         $.ajax({
             url:"/ruanganDeta",
@@ -14,5 +14,15 @@
                 // $("html").html(data)
             }
         })
+
+        // event.preventDefault();
+        // const xhttp = new XMLHttpRequest();
+        // xhttp.onreadystatechange = function() {
+        //     if (this.readyState == 4 && this.status == 200) {
+        //         $("html").html(this.responseText);
+        //     }
+        // };
+        // xhttp.open("GET", "/ruanganDeta", true);
+        // xhttp.send();
     }
 </script>
