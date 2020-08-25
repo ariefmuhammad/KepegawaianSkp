e00bd3d9192f9d49de3de40864648dbbdd84f185
masariuman <masariuman@gmail.com>
2020-08-25 21:39:47 +0700

    push modal jadi section

--- resources/views/admin/content/ruangan/index.blade.php
+++ resources/views/admin/content/ruangan/index.blade.php
@@ -44,6 +44,8 @@
 </div>
 <div>
     <button class="mr-2 mb-2 btn btn-primary" type="button">Tambah Ruangan Baru</button>
+    <button class="mr-2 mb-2 btn btn-primary" data-target="#onboardingWideFormModal" data-toggle="modal" type="button">Modal with form</button>
+
 </div>
 <div class="table-responsive">
     <table id="tabeldata" width="100%" class="table table-striped table-lightfont">
@@ -92,6 +94,51 @@
 @endpush
 
 
-@push('modal')
-
-@endpush
+@section('modal')
+{{-- modal tambah --}}
+<div aria-hidden="true" class="onboarding-modal modal fade animated" id="onboardingWideFormModal" role="dialog" tabindex="-1">
+  <div class="modal-dialog modal-lg modal-centered" role="document">
+    <div class="modal-content text-center">
+      <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Skip Intro</span><span class="os-icon os-icon-close"></span></button>
+      <div class="onboarding-side-by-side">
+        <div class="onboarding-media">
+          <img alt="" src="/iniTemplate/dist/img/bigicon5.png" width="200px">
+        </div>
+        <div class="onboarding-content with-gradient">
+          <h4 class="onboarding-title">
+            Example Request Information
+          </h4>
+          <div class="onboarding-text">
+            In this example you can see a form where you can request some additional information from the customer when they land on the app page.
+          </div>
+          <form>
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="">Your Full Name</label><input class="form-control" placeholder="Enter your full name..." type="text" value="">
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="">Your Role</label><select class="form-control">
+                    <option>
+                      Web Developer
+                    </option>
+                    <option>
+                      Business Owner
+                    </option>
+                    <option>
+                      Other
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+{{-- end modal tambah --}}
+@endsection
