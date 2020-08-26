7919f7db1268b80654802b2eeab857c50b1c6a60
masariuman <masariuman@gmail.com>
2020-08-26 15:31:38 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/content/pegawai/index.blade.php
+++ resources/views/admin/content/pegawai/index.blade.php
@@ -209,7 +209,7 @@
           <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
           <div class="onboarding-side-by-side">
             <div class="onboarding-media">
-              <img alt="" src="/iniTemplate/dist/img/bigicon5.png" width="200px">
+              <img alt="" src="/iniTemplate/dist/iconModal/pegawaiAdd.png" width="200px">
             </div>
             <div class="onboarding-content with-gradient">
               <h4 class="onboarding-title">
