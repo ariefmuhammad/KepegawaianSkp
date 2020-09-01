bfa10edc991b17bc3257512bb84e22cbc5936a69
ariefmuhammad <ituarief@gmail.com>
2020-09-01 17:40:40 +0700

    css pegawai

---
+++ resources/views/pegawai/part/css.blade.php
@@ -0,0 +1,8 @@
+<link href="{{asset('/iniTemplate/dist/bower_components/select2/dist/css/select2.min.css')}}" rel="stylesheet">
+<link href="{{asset('/iniTemplate/dist/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
+<link href="{{asset('/iniTemplate/dist/bower_components/dropzone/dist/dropzone.css')}}" rel="stylesheet">
+<link href="{{asset('/iniTemplate/dist/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('/iniTemplate/dist/bower_components/fullcalendar/dist/fullcalendar.min.css')}}" rel="stylesheet">
+<link href="{{asset('/iniTemplate/dist/bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css')}}" rel="stylesheet">
+<link href="'{{asset('/iniTemplate/dist/bower_components/slick-carousel/slick/slick.css')}}" rel="stylesheet">
+<link href="{{asset('/iniTemplate/dist/css/main.css?version=4.5.0')}}" rel="stylesheet">
