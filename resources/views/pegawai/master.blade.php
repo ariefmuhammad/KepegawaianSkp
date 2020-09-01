28035e451680083d8c898c3ca89a542ee28a3df0
ariefmuhammad <ituarief@gmail.com>
2020-09-01 17:39:26 +0700

    pegawai master

---
+++ resources/views/pegawai/master.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title')</title>
+    @include('pegawai.part.meta')
+    <link href="{{asset('/iniTemplate/dist/favicon.png')}}" rel="shortcut icon">
+    <link href="{{asset('/iniTemplate/dist/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet" type="text/css">
+    @include('pegawai.part.css')
+  </head>
+  <body class="menu-position-side menu-side-left full-screen">
+    <div class="all-wrapper solid-bg-all">
+      <div class="search-with-suggestions-w">
+        <div class="search-with-suggestions-modal">
+          <div class="element-search">
+            <input class="search-suggest-input" placeholder="Start typing to search..." type="text">
+              <div class="close-search-suggestions">
+                <i class="os-icon os-icon-x"></i>
+              </div>
+            </input>
+          </div>
+          <div class="search-suggestions-group">
+            <div class="ssg-header">
+              <div class="ssg-icon">
+                <div class="os-icon os-icon-box"></div>
+              </div>
+              <div class="ssg-name">
+                Projects
+              </div>
+              <div class="ssg-info">
+                24 Total
+              </div>
+            </div>
+            <div class="ssg-content">
+              <div class="ssg-items ssg-items-boxed">
+                <a class="ssg-item" href="users_profile_big.html">
+                  <div class="item-media" style="background-image: url(img/company6.png)"></div>
+                  <div class="item-name">
+                    Integ<span>ration</span> with API
+                  </div>
+                </a><a class="ssg-item" href="users_profile_big.html">
+                  <div class="item-media" style="background-image: url(img/company7.png)"></div>
+                  <div class="item-name">
+                    Deve<span>lopm</span>ent Project
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="search-suggestions-group">
+            <div class="ssg-header">
+              <div class="ssg-icon">
+                <div class="os-icon os-icon-users"></div>
+              </div>
+              <div class="ssg-name">
+                Customers
+              </div>
+              <div class="ssg-info">
+                12 Total
+              </div>
+            </div>
+            <div class="ssg-content">
+              <div class="ssg-items ssg-items-list">
+                <a class="ssg-item" href="users_profile_big.html">
+                  <div class="item-media" style="background-image: url({{asset('/iniTemplate/dist/img/avatar1.jpg')}})"></div>
+                  <div class="item-name">
+                    John Ma<span>yer</span>s
+                  </div>
+                </a><a class="ssg-item" href="users_profile_big.html">
+                  <div class="item-media" style="background-image: url({{asset('/iniTemplate/dist/img/avatar2.jpg')}})"></div>
+                  <div class="item-name">
+                    Th<span>omas</span> Mullier
+                  </div>
+                </a><a class="ssg-item" href="users_profile_big.html">
+                  <div class="item-media" style="background-image: url({{asset('/iniTemplate/dist/img/avatar3.jpg')}})"></div>
+                  <div class="item-name">
+                    Kim C<span>olli</span>ns
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="search-suggestions-group">
+            <div class="ssg-header">
+              <div class="ssg-icon">
+                <div class="os-icon os-icon-folder"></div>
+              </div>
+              <div class="ssg-name">
+                Files
+              </div>
+              <div class="ssg-info">
+                17 Total
+              </div>
+            </div>
+            <div class="ssg-content">
+              <div class="ssg-items ssg-items-blocks">
+                <a class="ssg-item" href="#">
+                  <div class="item-icon">
+                    <i class="os-icon os-icon-file-text"></i>
+                  </div>
+                  <div class="item-name">
+                    Work<span>Not</span>e.txt
+                  </div>
+                </a><a class="ssg-item" href="#">
+                  <div class="item-icon">
+                    <i class="os-icon os-icon-film"></i>
+                  </div>
+                  <div class="item-name">
+                    V<span>ideo</span>.avi
+                  </div>
+                </a><a class="ssg-item" href="#">
+                  <div class="item-icon">
+                    <i class="os-icon os-icon-database"></i>
+                  </div>
+                  <div class="item-name">
+                    User<span>Tabl</span>e.sql
+                  </div>
+                </a><a class="ssg-item" href="#">
+                  <div class="item-icon">
+                    <i class="os-icon os-icon-image"></i>
+                  </div>
+                  <div class="item-name">
+                    wed<span>din</span>g.jpg
+                  </div>
+                </a>
+              </div>
+              <div class="ssg-nothing-found">
+                <div class="icon-w">
+                  <i class="os-icon os-icon-eye-off"></i>
+                </div>
+                <span>No files were found. Try changing your query...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="layout-w">
+        @include('pegawai.part.mobile')
+        @include('pegawai.part.sidebar')
+        <div class="content-w">
+        @include('pegawai.part.topbar')
+        @include('pegawai.part.sublink')
+
+        @yield('content')
+
+        {{-- @include('pegawai.part.setting') --}}
+            {{-- </div>
+          </div> --}}
+        </div>
+      </div>
+      <div class="display-type"></div>
+    </div>
+    @include('pegawai.part.js')
+  </body>
+</html>
