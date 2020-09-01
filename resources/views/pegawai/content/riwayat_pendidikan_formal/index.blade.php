41d59b705a0ffbf4f064cd4b14d1e84940a95592
ariefmuhammad <ituarief@gmail.com>
2020-09-01 17:40:21 +0700

    riwayat pendidikan formal index

---
+++ resources/views/pegawai/content/riwayat_pendidikan_formal/index.blade.php
@@ -0,0 +1,341 @@
+@extends('pegawai.master')
+
+@push('css')
+@endpush
+
+
+@section('title')
+    Riwayat Pendidikan Formal
+@endsection
+
+
+@section('content')
+<div class="content-i">
+    <div class="content-box">
+      <div class="element-wrapper">
+        <h6 class="element-header">
+          Riwayat Pendidikan Formal
+        </h6>
+        <div class="element-box">
+          <h5 class="form-header">
+            Data Riwayat Pendidikan Formal
+          </h5>
+          <div class="form-desc">
+            Ini adalah data riwayat pendidikan formal anda, Silahkan di isi dengan lengkap.
+          </div>
+          <div class="table-responsive">
+            <table id="dataTable1" width="100%" class="table table-striped table-lightfont">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                    <th>Nama Diklat</th>
+                    <th>Tempat Belajar</th>
+                    <th>Lokasi</th>
+                    <th>Tanggal Mulai</th>
+                    <th>Tanggal Selesai</th>
+                    <th>Jumlah Jam</th>
+                    <th>Penyelenggara</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>
+                            No
+                        </th>
+                        <th>
+                            Name Diklat
+                        </th>
+                        <th>
+                            Tempat Belajar
+                        </th>
+                        <th>
+                            Lokasi
+                        </th>
+                        <th>
+                            Tanggal Mulai
+                        </th>
+                        <th>
+                            Tanggal Selesai
+                        </th>
+                        <th>
+                            Jumlah Jam
+                        </th>
+                        <th>
+                            Penyelenggara
+                        </th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                <tr>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        Tiger Nixon
+                    </td>
+                    <td>
+                        System Architect
+                    </td>
+                    <td>
+                        Edinburgh
+                    </td>
+                    <td>
+                        61
+                    </td>
+                    <td>
+                        2011/04/25
+                    </td>
+                    <td>
+                        $320,800
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        Garrett Winters
+                    </td>
+                    <td>
+                        Accountant
+                    </td>
+                    <td>
+                        Tokyo
+                    </td>
+                    <td>
+                        63
+                    </td>
+                    <td>
+                        2011/07/25
+                    </td>
+                    <td>
+                        $170,750
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+          </div>
+        </div>
+      </div><!--------------------
+      START - Color Scheme Toggler
+      -------------------->
+      <div class="floated-colors-btn second-floated-btn">
+        <div class="os-toggler-w">
+          <div class="os-toggler-i">
+            <div class="os-toggler-pill"></div>
+          </div>
+        </div>
+        <span>Dark </span><span>Colors</span>
+      </div>
+      <!--------------------
+      END - Color Scheme Toggler
+      --------------------><!--------------------
+      START - Demo Customizer
+      -------------------->
+      <div class="floated-customizer-btn third-floated-btn">
+        <div class="icon-w">
+          <i class="os-icon os-icon-ui-46"></i>
+        </div>
+        <span>Customizer</span>
+      </div>
+      <div class="floated-customizer-panel">
+        <div class="fcp-content">
+          <div class="close-customizer-btn">
+            <i class="os-icon os-icon-x"></i>
+          </div>
+          <div class="fcp-group">
+            <div class="fcp-group-header">
+              Menu Settings
+            </div>
+            <div class="fcp-group-contents">
+              <div class="fcp-field">
+                <label for="">Menu Position</label><select class="menu-position-selector">
+                  <option value="left">
+                    Left
+                  </option>
+                  <option value="right">
+                    Right
+                  </option>
+                  <option value="top">
+                    Top
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field">
+                <label for="">Menu Style</label><select class="menu-layout-selector">
+                  <option value="compact">
+                    Compact
+                  </option>
+                  <option value="full">
+                    Full
+                  </option>
+                  <option value="mini">
+                    Mini
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field with-image-selector-w">
+                <label for="">With Image</label><select class="with-image-selector">
+                  <option value="yes">
+                    Yes
+                  </option>
+                  <option value="no">
+                    No
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field">
+                <label for="">Menu Color</label>
+                <div class="fcp-colors menu-color-selector">
+                  <div class="color-selector menu-color-selector color-bright selected"></div>
+                  <div class="color-selector menu-color-selector color-dark"></div>
+                  <div class="color-selector menu-color-selector color-light"></div>
+                  <div class="color-selector menu-color-selector color-transparent"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="fcp-group">
+            <div class="fcp-group-header">
+              Sub Menu
+            </div>
+            <div class="fcp-group-contents">
+              <div class="fcp-field">
+                <label for="">Sub Menu Style</label><select class="sub-menu-style-selector">
+                  <option value="flyout">
+                    Flyout
+                  </option>
+                  <option value="inside">
+                    Inside/Click
+                  </option>
+                  <option value="over">
+                    Over
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field">
+                <label for="">Sub Menu Color</label>
+                <div class="fcp-colors">
+                  <div class="color-selector sub-menu-color-selector color-bright selected"></div>
+                  <div class="color-selector sub-menu-color-selector color-dark"></div>
+                  <div class="color-selector sub-menu-color-selector color-light"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="fcp-group">
+            <div class="fcp-group-header">
+              Other Settings
+            </div>
+            <div class="fcp-group-contents">
+              <div class="fcp-field">
+                <label for="">Full Screen?</label><select class="full-screen-selector">
+                  <option value="yes">
+                    Yes
+                  </option>
+                  <option value="no">
+                    No
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field">
+                <label for="">Show Top Bar</label><select class="top-bar-visibility-selector">
+                  <option value="yes">
+                    Yes
+                  </option>
+                  <option value="no">
+                    No
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field">
+                <label for="">Above Menu?</label><select class="top-bar-above-menu-selector">
+                  <option value="yes">
+                    Yes
+                  </option>
+                  <option value="no">
+                    No
+                  </option>
+                </select>
+              </div>
+              <div class="fcp-field">
+                <label for="">Top Bar Color</label>
+                <div class="fcp-colors">
+                  <div class="color-selector top-bar-color-selector color-bright selected"></div>
+                  <div class="color-selector top-bar-color-selector color-dark"></div>
+                  <div class="color-selector top-bar-color-selector color-light"></div>
+                  <div class="color-selector top-bar-color-selector color-transparent"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--------------------
+      END - Demo Customizer
+      --------------------><!--------------------
+      START - Chat Popup Box
+      -------------------->
+      <div class="floated-chat-btn">
+        <i class="os-icon os-icon-mail-07"></i><span>Demo Chat</span>
+      </div>
+      <div class="floated-chat-w">
+        <div class="floated-chat-i">
+          <div class="chat-close">
+            <i class="os-icon os-icon-close"></i>
+          </div>
+          <div class="chat-head">
+            <div class="user-w with-status status-green">
+              <div class="user-avatar-w">
+                <div class="user-avatar">
+                  <img alt="" src="img/avatar1.jpg">
+                </div>
+              </div>
+              <div class="user-name">
+                <h6 class="user-title">
+                  John Mayers
+                </h6>
+                <div class="user-role">
+                  Account Manager
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="chat-messages">
+            <div class="message">
+              <div class="message-content">
+                Hi, how can I help you?
+              </div>
+            </div>
+            <div class="date-break">
+              Mon 10:20am
+            </div>
+            <div class="message">
+              <div class="message-content">
+                Hi, my name is Mike, I will be happy to assist you
+              </div>
+            </div>
+            <div class="message self">
+              <div class="message-content">
+                Hi, I tried ordering this product and it keeps showing me error code.
+              </div>
+            </div>
+          </div>
+          <div class="chat-controls">
+            <input class="message-input" placeholder="Type your message here..." type="text">
+            <div class="chat-extra">
+              <a href="#"><span class="extra-tooltip">Attach Document</span><i class="os-icon os-icon-documents-07"></i></a><a href="#"><span class="extra-tooltip">Insert Photo</span><i class="os-icon os-icon-others-29"></i></a><a href="#"><span class="extra-tooltip">Upload Video</span><i class="os-icon os-icon-ui-51"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--------------------
+      END - Chat Popup Box
+      -------------------->
+    </div>
+  </div>
+@endsection
+
+@push('js')
+@endpush
+
