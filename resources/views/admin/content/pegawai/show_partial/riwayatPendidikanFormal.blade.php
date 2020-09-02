23de7942e7567ade08d7e27a7ac84a48e1f587c5
masariuman <masariuman@gmail.com>
2020-09-02 11:18:30 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/content/pegawai/show_partial/riwayatPendidikanFormal.blade.php
+++ resources/views/admin/content/pegawai/show_partial/riwayatPendidikanFormal.blade.php
@@ -19,7 +19,7 @@
                         <td class="text-center">{{$item->tingkat_pendidikan}}</td>
                         <td class="text-center">{{$item->nama_sekolah}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#riwayat_pendidikan_formal_show_{{$item->id}}"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data-target="#showDetail" data-toggle="modal"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -28,3 +28,43 @@
     </div>
 @endif
 
+
+
+
+{{-- modal --}}
+    <div aria-hidden="true" class="onboarding-modal modal fade animated" id="showDetail" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-centered" role="document">
+          <div class="modal-content text-center">
+            <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
+            <div class="onboarding-side-by-side">
+              <div class="onboarding-media">
+                <img alt="" src="/iniTemplate/dist/iconModal/ruanganAdd.png" width="200px">
+              </div>
+              <div class="onboarding-content with-gradient">
+                <h4 class="onboarding-title">
+                  Tambah Ruangan Baru
+                </h4>
+                <div class="onboarding-text">
+                  Masukkan nama ruangan baru yang ingin ditambahkan kedalam aplikasi.
+                </div>
+                <form id="addForm">
+                  @csrf
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="form-group">
+                          <input name="ruangan" id="inputAddRuangan" title="Nama Ruangan" placeholder="Masukkan Nama Ruangan Baru.." type="text" class="form-control" />
+                      </div>
+                    </div>
+                    <div class="col-sm-12">
+                      <div class="form-group text-center">
+                          <button class="mr-2 mb-2 btn btn-primary" data-target="#onboardingWideFormModal" data-toggle="modal" type="submit">Tambah Ruangan Baru</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+{{-- end modal --}}
