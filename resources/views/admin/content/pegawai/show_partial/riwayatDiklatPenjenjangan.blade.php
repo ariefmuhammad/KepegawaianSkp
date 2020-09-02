1839387393e54b3be7852122f72691ec35f85643
masariuman <masariuman@gmail.com>
2020-09-02 14:32:35 +0700

    diklat perjenjangan done

--- resources/views/admin/content/pegawai/show_partial/riwayatDiklatPenjenjangan.blade.php
+++ resources/views/admin/content/pegawai/show_partial/riwayatDiklatPenjenjangan.blade.php
@@ -19,7 +19,7 @@
                         <td class="text-center">{{$item->jenis_diklat}}</td>
                         <td class="text-center">{{$item->lokasi}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#riwayat_diklat_penjenjangan_show_{{$item->id}}"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data="{{$item}}"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -28,3 +28,83 @@
     </div>
 @endif
 
+<script>
+    $(document).ready(function() {
+        $('.modalTrigger').on('click',function() {
+            $('#showDetail').modal('show');
+            var data = JSON.parse($(this).attr("data"));
+            $("#jenisDiklat").html(data.jenis_diklat);
+            $("#dataAngkatan").html(data.angkatan);
+            $("#penyelenggara").html(data.penyelenggara);
+            $("#tanggalMulai").html(data.tanggal_mulai);
+            $("#tanggalSelesai").html(data.tanggal_selesai);
+            $("#predikat").html(data.predikat);
+            $("#lokasi").html(data.lokasi);
+            $("#jumlahJam").html(data.jumlah_jam);
+        });
+    });
+</script>
+
+
+
+
+
+
+
+{{-- modal --}}
+    <div aria-hidden="true" class="onboarding-modal modal fade animated" id="showDetail" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-centered masariuman-maxWidth1300px" role="document">
+          <div class="modal-content text-center">
+            <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
+            <div class="onboarding-side-by-side">
+              <div class="onboarding-media">
+                <img alt="" src="/iniTemplate/dist/iconModal/bigicon3.png" width="200px">
+              </div>
+              <div class="onboarding-content with-gradient masariuman-width1000px">
+                <h4 class="onboarding-title">
+                    Detail Data Riwayat Diklat Penjenjangan
+                </h4>
+                <div class="onboarding-text">
+                    <table class="mb-0 table table-striped">
+                        <tbody>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">JENIS DIKLAT</th>
+                        <td id="jenisDiklat"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">ANGKATAN</th>
+                        <td id="dataAngkatan"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">PENYELENGGARA</th>
+                        <td id="penyelenggara"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL MULAI</th>
+                        <td id="tanggalMulai"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL SELESAI</th>
+                        <td id="tanggalSelesai"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">PREDIKAT</th>
+                        <td id="predikat"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">LOKASI</th>
+                        <td id="lokasi"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">JUMLAH JAM</th>
+                        <td id="jumlahJam"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+{{-- end modal --}}
