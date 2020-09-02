012da74c4d16e1fe36567825a84c04859c04b1d6
masariuman <masariuman@gmail.com>
2020-09-02 15:50:57 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/content/pegawai/show_partial/riwayatDiklatKepangkatan.blade.php
+++ resources/views/admin/content/pegawai/show_partial/riwayatDiklatKepangkatan.blade.php
@@ -19,7 +19,7 @@
                         <td class="text-center">{{$item->nomor_sk}}</td>
                         <td class="text-center">{{$item->golongan}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#riwayat_kepangkatan_show_{{$item->id}}"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data="{{$item}}"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -28,3 +28,74 @@
     </div>
 @endif
 
+
+
+
+
+
+
+<script>
+    $(document).ready(function() {
+        $('.modalTrigger').on('click',function() {
+            $('#showDetail').modal('show');
+            var data = JSON.parse($(this).attr("data"));
+            $("#golongan").html(data.golongan);
+            $("#tmtGolongan").html(data.tmt_golongan);
+            $("#pejabatPenandatanganSk").html(data.pejabat_penandatangan_sk);
+            $("#nomorSk").html(data.nomor_sk);
+            $("#tanggalSk").html(data.tanggal_sk);
+        });
+    });
+</script>
+
+
+
+
+
+
+
+{{-- modal --}}
+    <div aria-hidden="true" class="onboarding-modal modal fade animated" id="showDetail" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-centered masariuman-maxWidth1300px" role="document">
+          <div class="modal-content text-center">
+            <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
+            <div class="onboarding-side-by-side">
+              <div class="onboarding-media">
+                <img alt="" src="/iniTemplate/dist/iconModal/bigicon3.png" width="200px">
+              </div>
+              <div class="onboarding-content with-gradient masariuman-width1000px">
+                <h4 class="onboarding-title">
+                    Detail Data Riwayat Kepangkatan
+                </h4>
+                <div class="onboarding-text">
+                    <table class="mb-0 table table-striped">
+                        <tbody>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">GOLONGAN/RUANG</th>
+                        <td id="golongan"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TMT GOLONGAN/RUANG</th>
+                        <td id="tmtGolongan"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">PEJABAT PENANDATANGAN SK</th>
+                        <td id="pejabatPenandatanganSk"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">NOMOR SK</th>
+                        <td id="nomorSk"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL SK</th>
+                        <td id="tanggalSk"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+{{-- end modal --}}
