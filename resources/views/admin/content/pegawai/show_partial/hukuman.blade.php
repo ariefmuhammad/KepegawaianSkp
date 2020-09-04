a0ad6b5bfeb7e6767a13de76d74f47001d5cc8e1
masariuman <masariuman@gmail.com>
2020-09-04 08:32:21 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/content/pegawai/show_partial/hukuman.blade.php
+++ resources/views/admin/content/pegawai/show_partial/hukuman.blade.php
@@ -19,7 +19,7 @@
                         <td class="text-center">{{$item->kode_hukuman}}</td>
                         <td class="text-center">{{$item->nomor_sk}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#hukum_show_{{$item->id}}"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data="{{$item}}"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -28,3 +28,76 @@
     </div>
 @endif
 
+
+
+
+
+
+
+
+
+<script>
+    $(document).ready(function() {
+        $('.modalTrigger').on('click',function() {
+            $('#showDetail').modal('show');
+            var data = JSON.parse($(this).attr("data"));
+            $("#kodeHukuman").html(data.kode_hukuman);
+            $("#nomorSk").html(data.nomor_sk);
+            $("#tanggalSk").html(data.tanggal_sk);
+            $("#tmtBerlaku").html(data.tmt_berlaku);
+            $("#pejabatPembuat_sk").html(data.pejabat_pembuat_sk);
+        });
+    });
+</script>
+
+
+
+
+
+
+
+{{-- modal --}}
+    <div aria-hidden="true" class="onboarding-modal modal fade animated" id="showDetail" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-centered masariuman-maxWidth1300px" role="document">
+          <div class="modal-content text-center">
+            <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
+            <div class="onboarding-side-by-side">
+              <div class="onboarding-media">
+                <img alt="" src="/iconModal/userCertificate.png" width="200px">
+              </div>
+              <div class="onboarding-content with-gradient masariuman-width1000px">
+                <h4 class="onboarding-title">
+                    Detail Data Hukuman Disiplin
+                </h4>
+                <div class="onboarding-text">
+                    <table class="mb-0 table table-striped">
+                        <tbody>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">KODE HUKUMAN</th>
+                        <td id="kodeHukuman"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">NOMOR SK</th>
+                        <td id="nomorSk"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL SK</th>
+                        <td id="tanggalSk"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TMT BERLAKU</th>
+                        <td id="tmtBerlaku"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">PEJABAT PEMBUAT SK</th>
+                        <td id="pejabatPembuat_sk"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+{{-- end modal --}}
