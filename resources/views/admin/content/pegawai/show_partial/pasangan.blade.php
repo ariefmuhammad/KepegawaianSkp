f679fe9ca60b6b650e64819f34c91e8dd08ee1bf
masariuman <masariuman@gmail.com>
2020-09-04 08:52:32 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/content/pegawai/show_partial/pasangan.blade.php
+++ resources/views/admin/content/pegawai/show_partial/pasangan.blade.php
@@ -61,7 +61,7 @@
             <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
             <div class="onboarding-side-by-side">
               <div class="onboarding-media">
-                <img alt="" src="/iconModal/userCertificate.png" width="200px">
+                <img alt="" src="/iconModal/couple.png" width="200px">
               </div>
               <div class="onboarding-content with-gradient masariuman-width1000px">
                 <h4 class="onboarding-title">
