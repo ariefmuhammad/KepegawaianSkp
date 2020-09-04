f774690b1151a149560cf57b18a26570b2bb79c7
masariuman <masariuman@gmail.com>
2020-09-04 08:42:29 +0700

    --- Auto-Git Commit ---

--- resources/views/admin/content/pegawai/show_partial/keluarga.blade.php
+++ resources/views/admin/content/pegawai/show_partial/keluarga.blade.php
@@ -21,7 +21,7 @@
                         <td class="text-center">{{$item->hubungan}}</td>
                         <td class="text-center">{{$item->pekerjaan}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#keluarga_kandung_show_{{$item->id}}"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data="{{$item}}"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -55,7 +55,7 @@
                         <td class="text-center">{{$item->hubungan}}</td>
                         <td class="text-center">{{$item->pekerjaan}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#keluarga_suami_show"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data="{{$item}}"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -64,3 +64,80 @@
     </div>
 @endif
 
+
+
+
+
+
+
+
+<script>
+    $(document).ready(function() {
+        $('.modalTrigger').on('click',function() {
+            $('#showDetail').modal('show');
+            var data = JSON.parse($(this).attr("data"));
+            $("#nama").html(data.nama);
+            $("#hubungan").html(data.hubungan);
+            $("#pekerjaan").html(data.pekerjaan);
+            $("#tanggal_lahir").html(data.tanggal_lahir);
+            $("#jenis_kelamin").html(data.jenis_kelamin);
+            $("#kondisi").html(data.kondisi);
+        });
+    });
+</script>
+
+
+
+
+
+
+
+{{-- modal --}}
+    <div aria-hidden="true" class="onboarding-modal modal fade animated" id="showDetail" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-centered masariuman-maxWidth1300px" role="document">
+          <div class="modal-content text-center">
+            <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
+            <div class="onboarding-side-by-side">
+              <div class="onboarding-media">
+                <img alt="" src="/iconModal/userCertificate.png" width="200px">
+              </div>
+              <div class="onboarding-content with-gradient masariuman-width1000px">
+                <h4 class="onboarding-title">
+                    Detail Data Keluarga
+                </h4>
+                <div class="onboarding-text">
+                    <table class="mb-0 table table-striped">
+                        <tbody>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">NAMA</th>
+                        <td id="nama"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">HUBUNGAN</th>
+                        <td id="hubungan"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">PEKERJAAN</th>
+                        <td id="pekerjaan"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL LAHIR</th>
+                        <td id="tanggal_lahir"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">JENIS KELAMIN</th>
+                        <td id="jenis_kelamin"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">KONDISI</th>
+                        <td id="kondisi"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+{{-- end modal --}}
--- resources/views/admin/content/pegawai/show_partial/organisasi.blade.php
+++ resources/views/admin/content/pegawai/show_partial/organisasi.blade.php
@@ -19,7 +19,7 @@
                         <td class="text-center">{{$item->nama_organisasi}}</td>
                         <td class="text-center">{{$item->kedudukan}}</td>
                         <td class="text-center">
-                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#organisasi_show_{{$item->id}}"><i class="fa fa-eye"></i></button>
+                            <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info modalTrigger" title="Lihat Data" data="{{$item}}"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -28,3 +28,82 @@
     </div>
 @endif
 
+
+
+
+
+<script>
+    $(document).ready(function() {
+        $('.modalTrigger').on('click',function() {
+            $('#showDetail').modal('show');
+            var data = JSON.parse($(this).attr("data"));
+            $("#tahunOrganisasi").html(data.tahun_organisasi);
+            $("#namaOrganisasi").html(data.nama_organisasi);
+            $("#kedudukan").html(data.kedudukan);
+            $("#tanggalMulai").html(data.tanggal_mulai);
+            $("#tanggalSelesai").html(data.tanggal_selesai);
+            $("#nomorSk").html(data.nomor_sk);
+            $("#jabatanPembuatSk").html(data.jabatan_pembuat_sk);
+        });
+    });
+</script>
+
+
+
+
+
+
+
+{{-- modal --}}
+    <div aria-hidden="true" class="onboarding-modal modal fade animated" id="showDetail" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-centered masariuman-maxWidth1300px" role="document">
+          <div class="modal-content text-center">
+            <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Tutup</span><span class="os-icon os-icon-close"></span></button>
+            <div class="onboarding-side-by-side">
+              <div class="onboarding-media">
+                <img alt="" src="/iconModal/userCertificate.png" width="200px">
+              </div>
+              <div class="onboarding-content with-gradient masariuman-width1000px">
+                <h4 class="onboarding-title">
+                    Detail Data Keanggotaan Organisasi
+                </h4>
+                <div class="onboarding-text">
+                    <table class="mb-0 table table-striped">
+                        <tbody>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TAHUN</th>
+                        <td id="tahunOrganisasi"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">NAMA ORGANISASI</th>
+                        <td id="namaOrganisasi"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">KEDUDUKAN DALAM ORGANISASI</th>
+                        <td id="kedudukan"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL MULAI</th>
+                        <td id="tanggalMulai"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">TANGGAL SELESAI</th>
+                        <td id="tanggalSelesai"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">NOMOR SK</th>
+                        <td id="nomorSk"></td>
+                        </tr>
+                        <tr>
+                        <th scope="row" class="masariuman-tdBgBlack">PEJABAT PEMBUAT SK</th>
+                        <td id="jabatanPembuatSk"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+{{-- end modal --}}
